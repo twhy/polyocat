638cef958de3b69e3ff87d7552a9f9e02f6fb52a
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-22 14:10:27 +0800

    It works

---
+++ polyocat.js
@@ -0,0 +1,73 @@
+(async function main() {
+  
+  let page = where()
+  let language = 'zh-CN' || navigator.language || navigator.languages[0]
+  let dictionary = await translation(language)
+
+  translate(document.body)
+
+  let observer = new MutationObserver((mutations) => {
+    mutations
+      .filter(mutation => mutation.type === 'childList' && mutation.addedNodes.length)
+      .forEach(mutation => mutation.addedNodes.forEach(translate))
+  })
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  })
+  
+  async function translation(lang) {
+    let url = chrome.extension.getURL(`translations/${lang}.json`)
+    let res = await fetch(url)
+    return await res.json()
+  }
+
+  function find(key) {
+    if (dictionary.global[key]) return ` ${dictionary.global[key]} `
+    if (dictionary[page][key]) return ` ${dictionary[page][key]} `
+  }
+  
+  function where() {
+    if (/^\/[\w\d]+\/?$/.test(location.pathname)) return 'profile'
+  }
+  
+  function translate(node) {
+    let childs = node.childNodes
+  
+    if (childs.length === 0) return
+  
+    childs.forEach(node => {
+      switch (true) {
+        case node.nodeType === Node.ELEMENT_NODE:
+          translateElement(node)
+          break
+        case node.nodeType === Node.TEXT_NODE:
+          translateTextNode(node)
+          break
+        default:
+          break;
+      }
+    })
+  }
+  
+  function translateElement(elem) {
+    if (elem.id === 'readme') return
+    if (elem.tagName === 'INPUT' && elem.placeholder) {
+      let placeholder = find(elem.placeholder)
+      if (placeholder) elem.placeholder = placeholder
+    }
+    
+    translate(elem)
+  }
+  
+  function translateTextNode(node) {
+    let text = node.nodeValue.trim()
+    let tran = find(text)
+    if (tran) {
+      node.nodeValue = tran
+    }
+  }
+
+})()
+
