400cde0c53e67ab493dd20cc9acc829e1cfd51b6
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-23 03:53:03 +0800

    More translations

--- polyocat.js
+++ polyocat.js
@@ -1,8 +1,10 @@
-(async function main() {
+async function main() {
   
   let page = where()
   let language = 'zh-CN' || navigator.language || navigator.languages[0]
   let dictionary = await translation(language)
+
+  console.log(page, language, dictionary)
 
   translate(document.body)
 
@@ -10,9 +12,14 @@
     mutations
       .filter(mutation => mutation.type === 'childList' && mutation.addedNodes.length)
       .forEach(mutation => mutation.addedNodes.forEach(translate))
+
+    mutations
+      .filter(mutation => mutation.type === 'attributes' && mutation.attributeName === 'placeholder')
+      .forEach(mutation => translate(mutation.target))
   })
 
   observer.observe(document.body, {
+    attributes: true,
     childList: true,
     subtree: true
   })
@@ -32,50 +39,54 @@
     let pathname = location.pathname
     if (pathname === '/') return 'homepage'
     if (pathname === '/new') return 'new'
+    if (/^\/watching/.test(pathname)) return 'watching'
+    if (/^\/notifications/.test(pathname)) return 'notifications'
+    if (/\/settings\/.+$/.test(pathname)) return 'settings'
     if (/^\/[\w\d]+\/?$/.test(pathname)) return 'profile'
     if (/^\/[\w\d]+\/.+$/.test(pathname)) return 'repository'
   }
   
   function translate(node) {
-    let childs = node.childNodes
-  
-    if (childs.length === 0) return
-  
-    childs.forEach(node => {
-      switch (true) {
-        case node.nodeType === Node.ELEMENT_NODE:
-          translateElement(node)
-          break
-        case node.nodeType === Node.TEXT_NODE:
-          translateTextNode(node)
-          break
-        default:
-          break;
-      }
-    })
+    translateNode(node)
+    node.childNodes.forEach(translate)
+  }
+
+  function translateNode(node) {
+    switch (true) {
+      case node.nodeType === Node.ELEMENT_NODE:
+        translateElement(node)
+        break
+      case node.nodeType === Node.TEXT_NODE:
+        translateTextNode(node)
+        break
+    }
   }
   
   function translateElement(elem) {
     if (elem.id === 'readme') return
-    if (elem.tagName === 'INPUT' && elem.placeholder) {
+    if ((elem.tagName === 'INPUT' || elem.tagName === 'TEXTAREA') && elem.placeholder) {
       let placeholder = find(elem.placeholder)
       if (placeholder) elem.placeholder = placeholder
+    }
+    if (elem.tagName === 'INPUT' && elem.type === 'submit') {
+      let value = find(elem.value)
+      if (value) elem.value = value
     }
     if (elem.hasAttribute('aria-label')) {
       let label = find(elem.getAttribute('aria-label'))
       if (label) elem.setAttribute('aria-label', label)
     }
-    
-    translate(elem)
   }
   
   function translateTextNode(node) {
-    let text = node.nodeValue.trim()
+    if (!node || node.nodeValue == null) return
+    let text = node.nodeValue.replace(/[\n\s]+/g, ' ').trim()
     let tran = find(text)
     if (tran) {
       node.nodeValue = tran
     }
   }
 
-})()
+}
 
+main()
