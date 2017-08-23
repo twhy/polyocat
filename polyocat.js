783516034467453bca27052c235465cd881269e3
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-23 12:32:37 +0800

    Handle relative time

--- polyocat.js
+++ polyocat.js
@@ -73,6 +73,12 @@ async function main() {
       let value = find(elem.value)
       if (value) elem.value = value
     }
+    if (elem.tagName === 'RELATIVE-TIME' && elem.innerText) {
+      elem.innerText = elem.innerText.replace(
+        /(\d+|a|an) (day|days|hour|hours|minute|minutes) (ago)/,
+        (match, p1, p2, p3) => `${find(p1) || p1} ${find(p2).trim()}${find(p3).trim()}`
+      )
+    }
     if (elem.hasAttribute('aria-label')) {
       let label = find(elem.getAttribute('aria-label'))
       if (label) elem.setAttribute('aria-label', label)
