6c8fc408bae2f35283ef9b73512622bf722f3220
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-29 01:33:21 +0800

    v0.0.3

--- release.sh
+++ release.sh
@@ -1,2 +1,10 @@
 crx pack -o polyocat.crx
 rm key.pem
+
+sed -i '' -e "4s/\(\"version\": \"\).\..\../\1$1/" manifest.json
+sed -i '' -e "3s/\(\"version\": \"\).\..\../\1$1/" package.json
+sed -i '' -e "3s/\(\"version\": \"\).\..\../\1$1/" package-lock.json
+
+git add .
+git commit -m "v$1"
+git tag v$1
